49f517a7bc8601e084a7bf730b9997325923890f
Daniil <tsarck2012@yandex.ru>
2023-12-18 22:22:15 +0300

    Update weather_requests.py

--- weather_requests.py
+++ weather_requests.py
@@ -11,6 +11,10 @@ def get_weather():
     send = []
     send.append("Сейчас в Москве " + str(grad) + skl(abs(grad)) + ", cкорость ветра " + str(api_response['current']['wind_speed'])+" м/c")
     send.append("Влажность " + str(api_response['current']['humidity']) +"%")
-    
     return send
+
+def skl(x) :
+    f1 = lambda a: (a%100)//10 != 1 and a%10 == 1
+    f2 = lambda a: (a%100)//10 != 1 and a%10 in [2,3,4]
+    return " градус" if f1(x) else  " градуса" if f2(x) else " градусов"
 
